64c2680d8f69a4bb08388bbe975448c0ff620799
joaoemanuels <jemanuel.pi@gmail.com>
2025-01-06 16:25:56 -0300

    remoção dos scripts de teste

--- tests/calculadora.test.js
+++
@@ -1,6 +0,0 @@
-const calculadora = require("../models/calculadora.js");
-
-test("somar 2 + 2 deveria retornar 4", () => {
-  const resultado = calculadora.somar(1, 3);
-  expect(resultado).toBe(4)
-});
