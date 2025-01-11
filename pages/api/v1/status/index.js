3715f5a018951f76058812a2ca3c7eaef059c37f
joaoemanuels <jemanuel.pi@gmail.com>
2025-01-11 16:36:09 -0300

    add `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import { Console } from "console";
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sun;  ");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor são legais e s@" });
 }
 
